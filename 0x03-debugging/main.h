4864af18d89043d058147fc10d0589ffa5835372
Clintsky <clintonoomondi@gmail.com>
2022-09-14 23:13:35 -0700

    2. 0 > 972?

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -7,30 +7,4 @@ void positive_or_negative(int i);
 
 int larget_number(int a, int b, int c)
 
-
-#include <stdio.h>
-#include "main.h"
-
-/**
-* main - prints the largest of 3 integers
-* Return: 0
-*/
-
-int main(void)
-{
-        int a, b, c;
-        int largest;
-
-        a = 0;
-        b = 0;
-        c = 0;
-
-        largest = largest_number(a, b, c);
-
-        printf("%d is the largest number\n", largest);
-
-        return (0);
-}
-
-
 #endif /* MAIN_H */
